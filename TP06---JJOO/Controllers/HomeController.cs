3c02a83d6e4fabc66fe7d2890a4eafd1d00d8800
PEDRO GERMAN GARCIA <48315562@ort>
2024-08-15 14:03:27 -0300

    1

---
+++ TP06---JJOO/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP06___JJOO.Models;
+
+namespace TP06___JJOO.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ TP06---JJOO/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP06___JJOO.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
